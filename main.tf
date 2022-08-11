50bf72f676944647d128453c26e46a45c79f0832
Joe Dillig <97898347+joe-at-torq@users.noreply.github.com>
2022-08-11 09:06:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,7 +92,8 @@ resource "azurerm_virtual_machine" "ubuntuserver_2004_vm" {
     computer_name  = "UbuntuServer"
     admin_username = "ubuntu"
     admin_password = "1qaz!QAZ1qaz!QAZ"
-    custom_data    = "${data.template_file.userdata.rendered}"
+    #custom_data   = "${data.template_file.userdata.rendered}"
+    custom_data    = "${file("userdata")}"
   }
   
     os_profile_linux_config {
